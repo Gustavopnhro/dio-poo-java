1381a7fc3060d43fd006de593608bc44db78a803
gustavo <gustavo.pnhro@gmail.com>
2023-09-09 22:23:42 -0300

    Organizando e entendendo o conceito de Comparator

---
+++ JavaCollections/ComparatorXComparable/Main.java
@@ -0,0 +1,40 @@
+package JavaCollections.ComparatorXComparable;
+
+import java.util.*;
+
+class Filme{
+    private String nome;
+    private int ano;
+
+    public Filme(String nome, int ano) {
+        this.nome = nome;
+        this.ano = ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Filme> filmes = new ArrayList<>();
+        filmes.add(new Filme("Harry Potter e a Pedra Filosofal", 2001));
+        filmes.add(new Filme("Efeito Borboleta", 2004));
+        filmes.add(new Filme("Mr.Nobody", 2003));
+
+        // Defina um Comparator personalizado para ordenar por nome
+        Comparator<Filme> porNome  = Comparator.comparing(Filme::getAno);
+
+        // Classifique as pessoas por nome
+        Collections.sort(filmes, porNome);
+
+        for (Filme filme : filmes) {
+            System.out.println(filme.getNome());
+        }
+    }
+}
