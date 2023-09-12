f0a0d087ee55790cef1e121d9f4b670c1fa0165f
gustavopnhro <gustavo.pnhro@gmail.com>
2023-09-12 13:40:07 -0300

    Entendendo as classes

---
+++ classe/src/School.java
@@ -0,0 +1,21 @@
+public class School {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "Claudio";
+        student1.age = 12;
+        student1.color = "Brown";
+        student1.sex = "Boy";
+
+        Student student2 = new Student();
+        student2.name = "Fernanda";
+        student2.age = 14;
+        student2.color = "Blonde";
+        student2.sex = "Girl";
+
+        Student student3 = new Student();
+        student3.name = "Nathalia";
+        student3.age = 11;
+        student3.color = "Redhead";
+        student3.sex = "Girl";
+    }
+}
---
+++ classe/src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    String name;
+    int age;
+    String color;
+    String sex;
+
+
+    void eating (String food){
+        System.out.println("Eating " + food);
+    }
+
+    void drinking (String drink){
+        System.out.println("Drinking " + drink);
+    }
+
+    void running (){
+        System.out.println("Sleeping");
+    }
+}
